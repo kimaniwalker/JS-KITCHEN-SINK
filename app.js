85726770167ca0b5d84e56024cefba1d0169a428
Kimani Walker <kimaniwalker@gmail.com>
2018-06-05 15:31:10 -0500

    Added variables

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('Hello World');
+
+var firstname = 'kimani';
+console.log(firstname);
+
+const states = 50;
+console.log(states);
+
+var x = 5;
+var y = 4;
+
+var addedsum = (x+y);
+console.log(addedsum);
